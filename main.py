ee25ac3975258dc52e3e31ada5732e86bd3c9f05
Ayulexx <80009013+Ayulexx@users.noreply.github.com>
2022-03-08 18:46:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ else:
   print("error device in the file")
   sys.exit()
 
-os.system('pip install -U TGDbot')
+os.system('pip install -U nlabot')
 os.system('clear')
 
-import TGDbot
+import nlabot
 
-client = TGDbot.PartyBot(
+client = nlabot.PartyBot(
   device_id=env_vars['DEVICE_ID'],
   account_id=env_vars['ACCOUNT_ID'],
   secret=env_vars['SECRET']
